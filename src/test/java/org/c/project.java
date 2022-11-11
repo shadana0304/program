8dbe2d8ba754574f77c6efa4c6b30468599c0401
Saranya <saranya3606@gmail.com>
2022-11-11 20:04:37 +0530

    First Time

---
+++ src/test/java/org/c/project.java
@@ -0,0 +1,30 @@
+package org.c;
+
+public class project {
+ private void method1() {
+	System.out.println("secA");
+}
+ 
+ private void method2() {
+	 System.out.println("secB");
+
+	}
+ 
+ private void method3() {
+	 System.out.println("secC");
+	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
